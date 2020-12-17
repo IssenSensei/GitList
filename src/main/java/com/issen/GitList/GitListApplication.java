765e524599fab569b5397094a11d5018c1847987
Krystian Demidiuk <issensensei@gmail.com>
2020-12-17 02:16:36 +0100

    Initial commit

---
+++ src/main/java/com/issen/GitList/GitListApplication.java
@@ -0,0 +1,13 @@
+package com.issen.GitList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/issen/GitList/GitListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.issen.GitList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
